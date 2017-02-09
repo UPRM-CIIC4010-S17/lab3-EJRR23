eb045f64c2cb1b63fd0ee919b6a8c0ba8ca7bcc2
s802097360 <s802097360@INCADELWS25.ecenet.ece.uprm.edu>
2017-02-09 12:59:26 -0400

    com & push

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,10 +3,12 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
+	
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		myFrame.setLocation(200, myFrame.WIDTH/2);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
 	
 	}
 }
